eca5d546d548d188ece8da1a50e9d6b4c647dcec
nickol <cnickol@gmail.com>
2020-10-19 12:39:31 +0700

    removed build_debug folder

--- build_debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/_local/otus_cpp_1_2
-# Build directory: C:/_local/otus_cpp_1_2/build_debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_ipfilter "test_ipfilter")
-set_tests_properties(test_ipfilter PROPERTIES  _BACKTRACE_TRIPLES "C:/_local/otus_cpp_1_2/CMakeLists.txt;91;add_test;C:/_local/otus_cpp_1_2/CMakeLists.txt;0;")
